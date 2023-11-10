961a90900f1edffcd49e980c442b2c1b85b3188d
Alberto Cangialosi <billyballo.res@gmail.com>
2023-11-10 16:11:18 +0100

    fix: hover with mouseover mouseleave
    for logo of its steve jobs academy

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  logoITS: string = '../../../assets/img/logo-sja-3.png';
 
+  logoITS_hover: string = '../../../assets/img/logo-sja-4.png'; // logo-sja-3 ma a colori "scambiati"
+
+  /**
+   * Per cambiare il path src del logo in caso di `:hover`, o meglio in caso di eventi `mouseover`, `mouseleft` di Angular
+   * @param isHovered : boolean, `true` per `mouseover`, `false` per `mouseleft` di Angular
+   */
+  itsHover(isHovered: boolean) {
+    this.logoITS = isHovered
+      ? this.logoITS_hover
+      : '../../../assets/img/logo-sja-3.png';
+
+    // DEBUG :
+    console.log('ITS Hover, Anakin');
+  }
 }
