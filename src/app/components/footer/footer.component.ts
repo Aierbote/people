e91a8a2175ebb8c2a1ee3dbb62344a42836c2624
Alberto Cangialosi <billyballo.res@gmail.com>
2023-11-10 17:40:43 +0100

    fix + feat: hover with mouseover
    Fixed the mouseleave for SteveJobsAcademy logo, now using a 'theirLogo' attr to store the current change
    
    Feature for profile picture / GitHub logo: with a brand new look for the logo and a credits of attribution for logo-gh-oneline

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,20 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  // per il banner/logo della Steve Jobs Academy
   logoITS: string = '../../../assets/img/logo-sja-3.png';
-
   logoITS_hover: string = '../../../assets/img/logo-sja-4.png'; // logo-sja-3 ma a colori "scambiati"
+  theirLogo!: string;
+
+  // per il logo GitHube/la profile picture mia
+  logoGitHub: string = 'https://pngimg.com/uploads/github/github_PNG91.png';
+  pfpAlberto: string = '../../../assets/img/alberto.jpg';
+  myLogo!: string;
+
+  ngOnInit(): void {
+    // copia iniziale per il loro logo
+    this.theirLogo = this.logoITS.slice();
+
+    // copia iniziale per il logo github
+    this.myLogo = this.logoGitHub.slice();
+  }
 
   /**
    * Per cambiare il path src del logo in caso di `:hover`, o meglio in caso di eventi `mouseover`, `mouseleft` di Angular
    * @param isHovered : boolean, `true` per `mouseover`, `false` per `mouseleft` di Angular
    */
   itsHover(isHovered: boolean) {
-    this.logoITS = isHovered
-      ? this.logoITS_hover
-      : '../../../assets/img/logo-sja-3.png';
+    this.theirLogo = isHovered ? this.logoITS_hover : this.logoITS;
 
     // DEBUG :
     console.log('ITS Hover, Anakin');
+  }
+
+  pfpHover(isHovered: boolean) {
+    this.myLogo = isHovered ? this.pfpAlberto : this.logoGitHub;
   }
 }
